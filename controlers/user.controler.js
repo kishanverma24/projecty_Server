65ca4377608702f6def26948cd5127ce56f59f3b
kishanverma24 <kishanverma9151@gmail.com>
2024-11-25 01:40:03 +0530

    user controller success message updated

--- controlers/user.controler.js
+++ controlers/user.controler.js
@@ -89,9 +89,11 @@ export const updateUserByUserId = async (req, res) => {
     if (!updatedUser) {
       return res.status(404).json({ message: "User not found" });
     }
-    res
-      .status(200)
-      .json({ message: "User updated successfully", currentUser: updatedUser });
+    res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+      currentUser: updatedUser,
+    });
   } catch (error) {
     res
       .status(400)
